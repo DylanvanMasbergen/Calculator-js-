896bc7ee82ce56eeaebe82054b4c7820a5eb6e8a
LAPTOP-AJDNNB9Q\dylan <99039655@mydavinci.nl>
2017-02-07 13:53:02 +0100

    Operators working now.

--- calc.js
+++ calc.js
@@ -1,9 +1,57 @@
+var getal1=0;
+var operator="";
+
+function calculate() {
+	var getalInDisplay = parseInt(document.getElementById('register').innerHTML);
+	if (operator == "+") {
+		var uitkomst = getal1 + getalInDisplay;
+    }
+    if (operator == "-") {
+    	var uitkomst = getal1 - getalInDisplay;
+    }
+    if (operator == "*") {
+    	var uitkomst = getal1 * getalInDisplay;
+    }
+    if (operator == "/") {
+    	var uitkomst = getal1 / getalInDisplay;
+    }
+
+	
+	document.getElementById('register').innerHTML = uitkomst;
+
+}
+
 function cls() {
 	register.innerHTML ='';
 }
+
 function test (parms){
   register.innerHTML+= parms.target.innerHTML;
 }
+
+function Plus() {
+	getal1 = parseInt(document.getElementById('register').innerHTML);
+	document.getElementById('register').innerHTML = '';
+	operator="+";
+	
+}
+function min() {
+	getal1 = parseInt(document.getElementById('register').innerHTML);
+	document.getElementById('register').innerHTML = '';
+	operator="-";
+}
+function deel() {
+	getal1 = parseInt(document.getElementById('register').innerHTML);
+	document.getElementById('register').innerHTML = '';
+	operator="/";
+}
+function keer() {
+	getal1 = parseInt(document.getElementById('register').innerHTML);
+	document.getElementById('register').innerHTML = '';
+	operator="*";
+}
+
+
 document.getElementById("btn1").addEventListener("click", test);
 document.getElementById("btn2").addEventListener("click", test);
 document.getElementById("btn3").addEventListener("click", test);
@@ -15,3 +63,14 @@ document.getElementById("btn8").addEventListener("click", test);
 document.getElementById("btn9").addEventListener("click", test);
 document.getElementById("btn0").addEventListener("click", test);
 document.getElementById("btnc").addEventListener("click", cls);
+
+document.getElementById("plus").addEventListener("click", Plus );
+document.getElementById("calculate").addEventListener("click", calculate );
+document.getElementById("min").addEventListener("click", min );
+document.getElementById("deel").addEventListener("click", deel );
+document.getElementById("keer").addEventListener("click", keer );
+//document.getElementById("opr5").addEventListener("click", );
+
+
+       
+      
